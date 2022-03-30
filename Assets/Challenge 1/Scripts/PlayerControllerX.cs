f251a9a55205259bd7a9aae0cf26edd3365009fa
Binqie <shamir.beishebaev@icloud.com>
2022-03-30 22:22:30 +0600

    first commit

---
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed = 3f;
+    public float rotationSpeed = 1f;
+    public float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+    }
+    void FixedUpdate()
+    {
+        // get the user's vertical input
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(new Vector3(1, 0, 0) * -verticalInput * rotationSpeed * Time.deltaTime);
+    }
+}
